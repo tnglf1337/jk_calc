60907728eebf896c0fa2dcab73b1759c8186534f
Timo Neske <Timo.Neske@hhu.de>
2025-10-07 11:32:46 +0200

    init project

---
+++ src/main/java/com/calc/JkCalcApplication.java
@@ -0,0 +1,13 @@
+package com.calc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JkCalcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JkCalcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/calc/JkCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JkCalcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
